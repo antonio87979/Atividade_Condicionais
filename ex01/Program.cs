fe81b90c413291b8bc85a6154fb42f09cb8ec39c
ANTONIO ROMEU MIGUEL DOS SANTOS NETO <antonio.rmsneto@senacsp.edu.br>
2025-01-15 21:46:41 -0300

    first commit

---
+++ ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿//Verificação de Paridade 
+//Objetivo: Praticar o uso do operador % (módulo) e estruturas condicionais simples. 
+//Descrição: Crie um programa que peça ao usuário para digitar um número inteiro. O programa deve verificar se o número é par ou ímpar. Utilize a condição if para determinar o resultado e exiba uma mensagem apropriada. 
+//Exemplo de Saída: "O número é par." ou "O número é ímpar."
+Console.WriteLine("digite um número");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"O número {num} é par.");
+}
+else
+{
+    Console.WriteLine($"o número {num} é impar");
+}
---
+++ ex02/Program.cs
@@ -0,0 +1,4 @@
+﻿//Número Positivo ou Negativo 
+//Objetivo: Aprender a trabalhar com condições múltiplas. 
+//Descrição: Desenvolva um programa que solicita ao usuário um número. O programa deve determinar se o número é positivo, negativo ou zero, utilizando if, else if, e else para as três possibilidades. Exiba a mensagem correspondente. 
+//Exemplo de Saída: "O número é positivo.", "O número é negativo." ou "O número é zero." 
