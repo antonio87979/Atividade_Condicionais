6252c32dfd9936cfdea0fc3a29fd9632ff8361d9
Antônio Romeu <antonio.rmsneto@senacsp.edu.br>
2025-01-20 15:28:20 -0300

    Update ex03

--- ex03/Program.cs
+++ ex03/Program.cs
@@ -4,3 +4,13 @@
 //nota for maior ou igual a 6, o aluno é considerado "Aprovado". Caso 
 //contrário, ele é "Reprovado". Exiba a situação do aluno na tela.
 //o Exemplo de Saída: "Aluno aprovado." ou "Aluno reprovado."
+
+		Console.WriteLine("Digite a nota do aluno(a)...");
+		int nota = int.Parse(Console.ReadLine());
+
+		if (nota > 10) {Console.WriteLine("Valor inválido");
+    }
+		else if (nota >= 6) {Console.WriteLine("Aluno APROVADO!");
+    }
+		else {Console.WriteLine("Aluno REPROVADO!");
+    }
