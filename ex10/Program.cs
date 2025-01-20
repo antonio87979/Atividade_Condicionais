b348f30c68d71bc524e55090286d990d5944fc8f
Antônio Romeu <antonio.rmsneto@senacsp.edu.br>
2025-01-20 15:41:37 -0300

    Update ex10

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -4,4 +4,21 @@
 //verifique se o valor é maior que 100. Se sim, aplique um desconto de 10% 
 //no valor e mostre o valor final. Caso contrário, exiba o valor original.
 //o Exemplo de Saída: "Valor com desconto: [valor final]." ou "Valor original: 
-//[valor
+//[valor]
+
+
+		Console.WriteLine("Digite o valor da compra:");
+		double valor = double.Parse(Console.ReadLine());
+
+		if (valor > 200 ) {
+		double desconto = valor * 0.20;
+		double valorfinal = valor - desconto;
+		Console.WriteLine($"O valor com desconto especial é: {valorfinal}.");
+  }
+		else if (valor > 100) {
+			double desconto2 = valor * 0.10;
+			double valorfinal2 = valor - desconto2;
+			Console.WriteLine($"O valor com desconto  é: {valorfinal2}.");
+	}
+		else {Console.WriteLine($"O valor final é: {valor}.");
+  }
